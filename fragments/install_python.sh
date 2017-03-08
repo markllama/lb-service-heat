e61811b855aad75126b205aab0bda9ce1b2f3e18
Mark Lamourine <markllama@gmail.com>
2017-03-08 16:31:23 -0500

    added more python required for fedora ansible

--- fragments/install_python.sh
+++ fragments/install_python.sh
@@ -2,4 +2,4 @@
 #
 # Ensure that Python is installed for Ansible
 # 
-[ -f /etc/fedora-release ] && dnf install -y python
+[ -f /etc/fedora-release ] && dnf install -y python python-dnf libselinux-python
